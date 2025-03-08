85fcc8d7d93d3ea108c8c64e1c08851fe101f767
JpCurada <johncurada.02@gmail.com>
2025-03-08 14:16:55 +0800

    Initial Project Setup

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="AWSCC Data Tool",
+    page_icon=":material/analytics:",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+with open( "static\styles.css" ) as css:
+    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+
+
+st.title("Dashboard Tool")
+
